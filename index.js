1701bf2f341c9be01df35d4f6f222883b751bcf1
Smoke5643 <smoke5643@gmail.com>
2022-12-19 21:20:02 -0500

    started adding questions to gather README data

--- index.js
+++ index.js
@@ -3,7 +3,100 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require ('./assets/javascript/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'This is a README generator. Please enter your full name:',
+        validate: nameInput => {
+            if (nameInput){
+                return true;
+            }else {
+                console.log('Please enter your name to give credit to you work!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your application:',
+        validate: titleInput => {
+            if (titleInput){
+                return true;
+            }else {
+                console.log('Please enter the title of your application!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a detailed description of your application:',
+        validate: descriptionInput => {
+            if (descriptionInput){
+                return true;
+            }else {
+                console.log('Please enter a description for your application!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Please enter instructions for the user to install:',
+        validate: instructionsInput => {
+            if (instructionsInput){
+                return true;
+            }else {
+                console.log('Please include installation instructions and all software or dependencies needed to use your application!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information:',
+        validate: usageInput => {
+            if (usageInput){
+                return true;
+            }else {
+                console.log('Please tell the user any relevant information to use your application!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter how others can contribute to this project:',
+        validate: contributionInput => {
+            if (contributionInput){
+                return true;
+            }else {
+                console.log('Please give users information on how to contribute to your project!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter the tests for your application and use cases:',
+        validate: testsInput => {
+            if (testsInput){
+                return true;
+            }else {
+                console.log('Please enter some testing information for the user!')
+                return false;
+            }
+        }
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
