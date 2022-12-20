0f81c30d8080084cce6728f5262323d521f24ac2
Smoke5643 <smoke5643@gmail.com>
2022-12-20 14:18:58 -0500

    most of generate markdown function is complete

--- utils/javascript/generateMarkdown.js
+++ utils/javascript/generateMarkdown.js
@@ -1,19 +1,55 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of Contents
+  - [Description](#description-of-project)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test Information](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+  
+  ## Description of project
+  
+  ${data.description}
+  
+  ## Installation
+  
+  ${data.instructions}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Contribution
+  
+  ${data.contribution}
+  
+  ## Test Information
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  If you have any questions about this project, please feel free to email me at ${data.email} or on Github at https://www.github.com/${data.gitHub}. I will do my best to get back to you as soon as possible!
+
+  ## License
+
+  I used the ${data.licenses} for this project.
 `;
 }
 
